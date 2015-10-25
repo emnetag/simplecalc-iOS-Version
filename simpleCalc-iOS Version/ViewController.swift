bb77d0a4ea74d40cc25a11132323d377027216db
Emnet Gossaye <emnetg@uw.edu>
2015-10-25 01:25:22 -0700

    additon operation complete

--- simpleCalc-iOS Version/ViewController.swift
+++ simpleCalc-iOS Version/ViewController.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
+enum MathOps {
+    case ADD
+    case SUB
+    case MULT
+    case MOD
+    case FACT
+    case AVG
+    case COUNT
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var navBar: UINavigationItem!
@@ -25,10 +35,83 @@ class ViewController: UIViewController {
     @IBOutlet weak var three: UIButton!
     @IBOutlet weak var zero: UIButton!
     
-    @IBOutlet var nums: [UIButton]!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var currentOp: MathOps!
+    
+    var leftHand: Int = 0
+    var rightHand: Int = 0
+    
+    var currentLabel: String?
+    
+    var equalsPressed: Bool = false
+    
+    func convertDouble(incoming: String) -> Double {
+        return NSNumberFormatter().numberFromString(incoming)!.doubleValue
+    }
+    
+    func convertInt(incoming: String) -> Int {
+        return NSNumberFormatter().numberFromString(incoming)!.integerValue
+    }
     
     @IBAction func buttonPressed(sender: UIButton, forevent: UIEvent) {
-        print("Just pressed \(sender.titleLabel!.text!)")
+        if self.inputLabel != nil {
+            let buttonString = sender.titleLabel!.text!
+            let labelString = self.inputLabel!.text!
+            
+            if (labelString == "0" || equalsPressed ) {
+                self.inputLabel!.text! = buttonString
+                equalsPressed = false
+            } else {
+                self.inputLabel!.text!.appendContentsOf(buttonString)
+            }
+        }
+    }
+
+    @IBAction func addition(sender: UIButton) {
+        self.leftHand = convertInt(self.inputLabel!.text!)
+        self.inputLabel!.text! = "0"
+        
+        if self.currentOp == nil {
+            self.currentOp = MathOps.ADD
+        }
+        self.currentOp! = MathOps.ADD
+    }
+    
+    @IBAction func equals(sender: UIButton) {
+        self.rightHand = convertInt(self.inputLabel!.text!)
+        
+        if self.currentOp != nil {
+            switch self.currentOp! {
+                case .ADD:
+                    self.inputLabel!.text! = String(self.leftHand + self.rightHand)
+                case .SUB:
+                    self.inputLabel!.text! = String(self.leftHand - self.rightHand)
+                case .COUNT:
+                    print("Not sure what to do yet")
+                case .AVG:
+                    print("Not sure what to do yet")
+                case .MOD:
+                    self.inputLabel!.text! = String(self.leftHand % self.rightHand)
+                case .FACT:
+                    let result = factorial(convertInt(self.inputLabel!.text!))
+                    self.inputLabel!.text! = String(result)
+                case .MULT:
+                    self.inputLabel!.text! = String(self.leftHand * self.rightHand)
+            }
+        }
+        self.equalsPressed = true
+    }
+    
+    func factorial(n: Int) -> Int {
+        if n == 0 {
+            return 1
+        }
+        var fact = 1
+        for i in 1...n {
+            fact = fact * i
+        }
+        return fact
     }
     
     override func viewDidLoad() {
